e36ec337727264b0968643d417c2d233f981d11c
wngmly324 <103935831+wngmly324@users.noreply.github.com>
2023-02-27 15:02:35 +0900

    1단계_입출력과 사칙연산

---
+++ Baekjoon/1_입출력과사칙연산/2588.py
@@ -0,0 +1,13 @@
+n1 = input()
+n2 = input()
+
+res1 = int(n1) * int(n2[-1])
+print(res1)
+
+res2 = int(n1) * int(n2[1])
+print(res2)
+
+res3 = int(n1) * int(n2[0])
+print(res3)
+
+print(int(n1) * int(n2))
