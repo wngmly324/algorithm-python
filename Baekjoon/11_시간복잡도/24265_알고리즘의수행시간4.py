fe1477dc143031193e6b7cd21cd1f77c71ba43a0
wngmly324 <103935831+wngmly324@users.noreply.github.com>
2023-04-29 12:03:20 +0900

    11단계_시간 복잡도

---
+++ Baekjoon/11_시간복잡도/24265_알고리즘의수행시간4.py
@@ -0,0 +1,12 @@
+n = int(input())
+print("%d" % (n * (n - 1) / 2))
+print(2)
+
+# n = int(input())
+# sum= 0
+# for i in range(1, n, 1):
+#     for j in range(i+1, n+1, 1):
+#         print(i, j)
+#         sum += 1
+# print(sum)
+# print(2)
