6f18454fab6e8f8c9b3bb5424479b993eda78a36
wngmly324 <103935831+wngmly324@users.noreply.github.com>
2023-06-02 09:37:11 +0900

    틀린 문제 복습

---
+++ Baekjoon/틀렸던문제/2581_소수.py
@@ -0,0 +1,18 @@
+n = int(input())
+m = int(input())
+data = []
+
+for i in range(n, m+1):
+    no = 0
+    if i > 1:
+        for j in range(2, i):
+            if i % j == 0:
+                no += 1
+        if no == 0:
+            data.append(i)
+
+if len(data) == 0:
+    print(-1)
+else:
+    print(sum(data))
+    print(min(data))
