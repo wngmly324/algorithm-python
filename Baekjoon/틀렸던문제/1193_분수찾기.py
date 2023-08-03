f0b9eaaf1ab24743bb187c91940756effb84bebf
wngmly324 <103935831+wngmly324@users.noreply.github.com>
2023-08-03 11:07:58 +0900

    틀린 문제 복습

--- Baekjoon/틀렸던문제/1193_분수찾기.py
+++ Baekjoon/틀렸던문제/1193_분수찾기.py
@@ -1,17 +1,15 @@
 x = int(input())
 
-# n -= line을 하면 각 line에서 n이 몇 번째에 위치하는지 알 수 있다
 line = 1
 while x > line:
-    x -= line
+    x -= line           # n -= line을 하면 각 line에서 n이 몇 번째에 위치하는지 알 수 있다
     line += 1
 
-# line이 짝수일 때와 홀수이 때 분모, 분자의 증감 양상이 다르다
-if line % 2 == 0:           # 짝수일 때는 분모 -1씩, 분자 +1씩
-    up = x
-    down = line - x + 1
-else:                       # 홀수일 때는 분모 +1씩, 분자 -1씩
-    up = line - x + 1
-    down = x
+if line % 2 == 0:       # 짝수일 때는 분모 -1씩, 분자 +1씩
+    a = x
+    b = line - x + 1
+else:                   # 홀수일 때는 분모 +1씩, 분자 -1씩
+    a = line - x + 1
+    b = x
 
-print("%d/%d" % (up, down))
+print("%d/%d" % (a, b))
