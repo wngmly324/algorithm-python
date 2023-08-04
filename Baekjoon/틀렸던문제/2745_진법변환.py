c4c94dfc4f64a4c6dc4aa819fc99e93a2180934d
wngmly324 <103935831+wngmly324@users.noreply.github.com>
2023-08-04 09:16:16 +0900

    틀린 문제 복습

--- Baekjoon/틀렸던문제/2745_진법변환.py
+++ Baekjoon/틀렸던문제/2745_진법변환.py
@@ -1,4 +1,15 @@
 n, b = input().split()
 
-n = int(n, int(b))
-print(n)
+print(int(n, int(b)))
+
+#######################
+
+a = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+n, b = input().split()
+
+n = n[::-1]
+ans = 0
+for i in range(len(n)):
+    ans += a.find(n[i]) * int(b) ** i
+
+print(ans)
