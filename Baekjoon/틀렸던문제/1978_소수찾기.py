1a338268eaaa4acec95a738850a48e8c8742f12b
wngmly324 <103935831+wngmly324@users.noreply.github.com>
2023-08-11 12:08:24 +0900

    틀린 문제 복습

--- Baekjoon/틀렸던문제/1978_소수찾기.py
+++ Baekjoon/틀렸던문제/1978_소수찾기.py
@@ -1,14 +1,16 @@
 n = int(input())
-data = list(map(int, input().split()))
+num_list = list(map(int, input().split()))
 
 cnt = 0
-for i in data:
+
+for i in num_list:
     no = 0
     if i > 1:
-        for j in range(2, i):
+        for j in range(2, i+1):
             if i % j == 0:
                 no += 1
-        if no == 0:
+
+        if no == 1:
             cnt += 1
 
 print(cnt)
