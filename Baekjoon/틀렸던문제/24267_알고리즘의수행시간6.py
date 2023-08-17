a19ba254c85ba68b9ab625d38e58d7ce2cb7b64e
wngmly324 <103935831+wngmly324@users.noreply.github.com>
2023-08-17 09:35:02 +0900

    틀린 문제 복습

--- Baekjoon/틀렸던문제/24267_알고리즘의수행시간6.py
+++ Baekjoon/틀렸던문제/24267_알고리즘의수행시간6.py
@@ -1,9 +1,8 @@
 n = int(input())
-print((n * (n-1) * (n-2)) // 6)
+
+print(int(n*(n-1)*(n-2) / 6))
 print(3)
 
 # 1부터 n까지의 숫자 중 3가지를 뽑아 중복 없이, 크기 순으로 작성하는 경우의 수가 수행 횟수
 # nC3 = n! / (n-3)! * 3!
 # 계산하면, (n-2)(n-1)n / 6
-
-# https://dev-mandos.tistory.com/124
